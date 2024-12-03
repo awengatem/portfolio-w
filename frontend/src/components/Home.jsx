c3ecaf3f53104dd16d66cfe45da0c0895c7f4c7a
awengatem <awengjohnatem@gmail.com>
2024-12-03 18:22:31 +0300

    Updated home section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,13 +4,16 @@ import myProfile from '../assets/profile.jpeg';
 const Home = () => {
   return (
     <div>
-      <div className="w-full h-screen flex flex-col items-center justify-center scale-75 md:flex-row md:scale-50 lg:flex-row lg:scale-50">
-        <img src={myProfile} alt="" />
-        <ul className="flex items-center gap-[4rem] text-[1.5rem] md:flex-col md:text-[3rem] lg:gap-[5rem] lg:text-[3rem]">
+      <div className="group relative w-full h-screen flex flex-col items-center justify-center scale-75 gap-[5rem] md:flex-row md:scale-50 md:gap-[10rem] lg:flex-row lg:scale-50">
+        {/* Image */}
+        <img className="cursor-pointer border-2" src={myProfile} alt="" />
+
+        {/* list */}
+        <ul className="flex items-center gap-[4rem] text-[2rem] md:flex-col md:text-[3rem] lg:gap-[5rem] lg:text-[3rem] md:opacity-0 md:transition-opacity md:duration-300 md:group-hover:opacity-100">
           <NavLink to="/">HOME</NavLink>
-          <NavLink to="/">ABOUT</NavLink>
-          <NavLink to="/">WORK</NavLink>
-          <NavLink to="/">CONTACT</NavLink>
+          <NavLink to="/about">ABOUT</NavLink>
+          <NavLink to="/work">WORK</NavLink>
+          <NavLink to="/contact">CONTACT</NavLink>
         </ul>
       </div>
     </div>
