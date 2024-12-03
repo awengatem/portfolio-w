9f2c71267c2ec1fbd8a2e99ee4fc2d9fc382802a
awengatem <awengjohnatem@gmail.com>
2024-12-03 13:54:42 +0300

    updated home section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,26 +3,15 @@ import myProfile from '../assets/profile.jpeg';
 
 const Home = () => {
   return (
-    <div className="transition-duration: 150ms">
-      <div className="flex flex-col items-center justify-center scale-90 md:flex-row md:scale-75 md:w-full h-screen lg:scale-75 group">
-        {/* Image Section */}
-        <div className="md:hover:animate-move-left">
-          <img
-            className="cursor-pointer lg:scale-90"
-            src={myProfile}
-            alt="Profile"
-          />
-        </div>
-
-        {/* Hidden UL Section with Fixed Position on Large Screens */}
-        <div className="hidden group-hover:block lg:fixed lg:right-0 lg:top-1/2 lg:-translate-y-1/2 lg:mt-[30px] lg:mr-auto">
-          <ul className="flex items-center justify-center gap-4 text-3xl uppercase md:text-4xl md:flex-col md:gap-[5rem] lg:text-6xl lg:animate-move-down">
-            <NavLink to="/home">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/work">Work</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-          </ul>
-        </div>
+    <div>
+      <div className="w-full h-screen flex flex-col items-center justify-center scale-75 md:flex-row md:scale-50 lg:flex-row lg:scale-50">
+        <img src={myProfile} alt="" />
+        <ul className="flex items-center gap-[4rem] text-[1.5rem] md:flex-col md:text-[3rem] lg:gap-[5rem] lg:text-[3rem]">
+          <NavLink to="/">HOME</NavLink>
+          <NavLink to="/">ABOUT</NavLink>
+          <NavLink to="/">WORK</NavLink>
+          <NavLink to="/">CONTACT</NavLink>
+        </ul>
       </div>
     </div>
   );
