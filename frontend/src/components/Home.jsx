9d99dd5ea165b3c37aa202ff48817761652e4d7c
awengatem <awengjohnatem@gmail.com>
2024-12-03 04:02:38 +0300

    edited home section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,15 +4,19 @@ import myProfile from '../assets/profile.jpeg';
 const Home = () => {
   return (
     <div className="transition-duration: 150ms">
-      <div className="flex flex-col items-center justify-center scale-90 md:flex-row md:scale-75 md:w-full md:h-screen lg:scale-75 group">
+      <div className="flex flex-col items-center justify-center scale-90 md:flex-row md:scale-75 md:w-full h-screen lg:scale-75 group">
         {/* Image Section */}
-        <div>
-          <img className="cursor-pointer" src={myProfile} alt="Profile" />
+        <div className="md:hover:animate-move-left">
+          <img
+            className="cursor-pointer lg:scale-90"
+            src={myProfile}
+            alt="Profile"
+          />
         </div>
 
-        {/* Hidden UL Section */}
-        <div className="hidden group-hover:block opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
-          <ul className="flex items-center justify-center gap-4 text-3xl md:text-4xl md:flex-col md:gap-[5rem] lg:text-6xl">
+        {/* Hidden UL Section with Fixed Position on Large Screens */}
+        <div className="hidden group-hover:block lg:fixed lg:right-0 lg:top-1/2 lg:-translate-y-1/2 lg:mt-[30px] lg:mr-auto">
+          <ul className="flex items-center justify-center gap-4 text-3xl uppercase md:text-4xl md:flex-col md:gap-[5rem] lg:text-6xl lg:animate-move-down">
             <NavLink to="/home">Home</NavLink>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/work">Work</NavLink>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,36 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'move-left': 'moveLeft 1s ease-in-out',
+        'move-down': 'moveDown 1s ease-in-out',
+        'move-right': 'moveLeft 1s ease-in-out',
+      },
+      keyframes: {
+        moveLeft: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(100px)',
+          },
+          '80%': {
+            transform: 'translateX(-20px)',
+          },
+          '100%': {
+            opacity: '1',
+          },
+        },
+        moveDown: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(30px)',
+          },
+          '100%': {
+            opacity: '1',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
