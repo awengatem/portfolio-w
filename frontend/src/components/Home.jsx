168472a71da5e2b4e0b8ea3ba45b14a8b07b6806
awengatem <awengjohnatem@gmail.com>
2024-12-04 16:49:15 +0300

    updated home section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,15 +6,21 @@ const Home = () => {
     <div>
       <div className="group relative w-full h-screen flex flex-col items-center justify-center scale-75 gap-[5rem] md:flex-row md:scale-50 md:gap-[10rem] lg:flex-row lg:scale-50">
         {/* Image */}
-        <img className="cursor-pointer border-2" src={myProfile} alt="" />
+        <img
+          className="cursor-pointer border-2 md:transition-all md:duration-300 md:group-hover:scale-110"
+          src={myProfile}
+          alt=""
+        />
 
         {/* list */}
-        <ul className="flex items-center gap-[4rem] text-[2rem] md:flex-col md:text-[3rem] lg:gap-[5rem] lg:text-[3rem] md:opacity-0 md:transition-opacity md:duration-300 md:group-hover:opacity-100">
-          <NavLink to="/">HOME</NavLink>
-          <NavLink to="/about">ABOUT</NavLink>
-          <NavLink to="/work">WORK</NavLink>
-          <NavLink to="/contact">CONTACT</NavLink>
-        </ul>
+        <div className="md:transform md:translate-x-0 md:group-hover:translate-x-10 md:transition-transform md:duration-300">
+          <ul className="flex items-center gap-[4rem] text-[2rem] md:flex-col md:text-[3rem] lg:gap-[5rem] lg:text-[3rem] md:opacity-0 md:transition-opacity md:duration-300 md:group-hover:opacity-100">
+            <NavLink to="/">HOME</NavLink>
+            <NavLink to="/about">ABOUT</NavLink>
+            <NavLink to="/work">WORK</NavLink>
+            <NavLink to="/contact">CONTACT</NavLink>
+          </ul>
+        </div>
       </div>
     </div>
   );
